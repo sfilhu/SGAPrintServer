1fc6b7a042d087c27f3e872d0f1815b6201fb558
sfilhu <sfilhu@gmail.com>
2020-04-07 18:41:02 +0000

    Start servidor https - ok

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 console.log(123)
-// import Vue from 'vue'
+import Vue from 'vue'
 // import VueMaterial from 'vue-material'
 // import 'vue-material/dist/vue-material.min.css'
-// import App from './App.vue'
+import App from './App.vue'
 
 // require('dotenv').config();
 const express  = require('express');
 const fs       = require('fs');
 const path     = require('path');
-// const morgan   = require('morgan');
-// const cors     = require('cors');
+const morgan   = require('morgan');
+const cors     = require('cors');
 const routes   = require('./routes');
 const app      = new express();
 
@@ -22,26 +22,26 @@ const httpsOpt = {
 }
 
 app
-// .use(cors())
-// .use(express.json())
-// .use(express.urlencoded({ extended: true }))
-// .use(morgan('dev'))
-// .use( (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// })
+.use(cors())
+.use(express.json())
+.use(express.urlencoded({ extended: true }))
+.use(morgan('dev'))
+.use( (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 .use(routes)
 
 
 const server = require('https').Server(httpsOpt, app);
 server.listen(3001)
 
-// Vue.config.productionTip = false
+Vue.config.productionTip = false
 
 // Vue
 // .use(VueMaterial)
 
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+new Vue({
+     render: h => h(App),
+}).$mount('#app')
